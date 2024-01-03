709c8c6315d2029ef0bd3502d19c980191e5a0f9
Alla Chernova <chall2005@yandex.ru>
2024-01-03 17:52:01 +0300

    title page is okay and connected to the second one!!

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,8 @@
-function myFunction(){
-    window.location.href = 'https://www.youtube.com/watch?v=PgnAYzEjhmo';
-    // window.location.href = '.\main_page.html';
-}
+// function myFunction(){
+//     window.location.href = 'https://www.youtube.com/watch?v=PgnAYzEjhmo';
+//     // window.location.href = '.\main_page.html';
+// }
+
+document.getElementById("myButton").addEventListener("click", function() {
+    window.location.href = "main_page.html";
+  });
