37f60ea8a34e65345e6345fa09c9f49d08dee661
Alla Chernova <chall2005@yandex.ru>
2024-05-14 16:35:49 +0300

    BERT code without trained_model.pt

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\chern\OneDrive\Рабочий стол\Курсовая СППР web\.venv
+set VIRTUAL_ENV=c:\Users\chern\KURSOVAYA\Decision-support-system\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
