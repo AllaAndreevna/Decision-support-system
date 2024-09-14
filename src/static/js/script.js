e95b3b3771d7c98833f05e4f4773709c3ba5f529
Alla Chernova <chall2005@yandex.ru>
2024-09-15 02:05:55 +0300

    Version 3.1

--- src/static/js/script.js
+++
@@ -1,9 +0,0 @@
-// function myFunction(){
-//     window.location.href = 'https://www.youtube.com/watch?v=PgnAYzEjhmo';
-//     // window.location.href = '.\main_page.html';
-// }
-
-// document.getElementById("myButton").addEventListener("click", function() {
-//     window.location.href = "main_page.html";
-//   });
-
